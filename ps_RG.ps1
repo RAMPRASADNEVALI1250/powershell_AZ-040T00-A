a48d294cd07d40c40bb810510feb53ac6f0dee4e
RAMPRASADNEVALI1250 <ram.prasad.nevali@gmail.com>
2025-08-28 21:55:20 +0530

    Initial commit

---
+++ ps_RG.ps1
@@ -0,0 +1,45 @@
+﻿#Connect-AzAccount -Tenant "86c18b2a-5e00-4f70-af18-d39bb64a6c49"
+
+function azure-rg {
+   param (
+    #[Parameter(Mandatory = $true)]
+    [string] $RG_listpath = "D:\ps_practice\RG_list.txt",
+
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("create", "delete")]
+    [string]$Action
+   )
+   $location = "canadacentral"
+    $existingRG = Get-AzResourceGroup | Select-Object -ExpandProperty ResourceGroupName
+
+    $requiredRG = Get-Content -Path $RG_listpath
+    switch ($Action)
+       {
+           "create" {
+            foreach ($rg in $requiredRG){
+                if ($rg -notin $existingRG){
+                    New-AzResourceGroup -Name $rg -Location $location
+                } else {
+                    Write-Host "RG $rg already exits" -ForegroundColor Green
+                }}
+                }
+           "delete" {
+                foreach ($rg in $requiredRG){
+                if ($rg -in $requiredRG){
+                    Remove-AzResourceGroup -Name $rg -Force
+                } else {
+                    Write-Host "RG $rg doesnt exits in Azure" -ForegroundColor Yellow
+                }
+           }
+           }
+           default {
+            Write-Host "Provide either create or delete only" 
+            }
+       }
+    
+
+    }
+
+
+Measure-Command { azure-rg -Action create}
+Measure-Command {azure-rg -Action delete}
---
+++ ps_vnet.ps1
@@ -0,0 +1,54 @@
+﻿param (
+    [Parameter(Mandatory = $true)]
+    [string]$vnet_name,
+
+    [Parameter(Mandatory = $true)]
+    [string]$rg_name,
+
+    [Parameter(Mandatory = $true)]
+    [string]$vnet_location,
+
+    [string]$addr_prefix = "10.0.0.0/16"
+    )
+
+function check-vnetExist {
+    
+    param (
+    [Parameter(Mandatory = $true)]
+    [string]$vnet_name,
+
+    [Parameter(Mandatory = $true)]
+    [string]$rg_name
+    )
+
+    if (Get-AzResourceGroup -name $rg_name -ErrorAction SilentlyContinue){
+        if (Get-AzVirtualNetwork -name $vnet_name -ResourceGroupName $rg_name -ErrorAction SilentlyContinue){
+            Write-Host "Vnets $vnet_name exists in Rg $rg_name"
+            return $true
+        }
+    } else {
+        Write-Host "Resource group $rg_name doesnt exists"
+        return $false
+    }
+}
+
+$tags = @{ 
+            env= "dev"
+            owner = "ram"
+        }
+
+$subnet = New-AzVirtualNetworkSubnetConfig -Name "subnet1" -AddressPrefix "10.0.0.0/24"
+
+if (!(check-vnetExist $vnet_name $rg_name )) {
+    New-AzVirtualNetwork `
+        -Name $vnet_name -ResourceGroupName $rg_name -Location $vnet_location -Tag $tags `
+        -AddressPrefix $addr_prefix -Subnet $subnet
+    #Write-Host "Vnet $vnet_name creating...."
+
+} else {
+    Write-Host "Vnet $vnet_name already exits"
+}
+
+#Remove-AzVirtualNetwork -Name dev-test -ResourceGroupName Role-Test -Force
+
+#.\ps_vnet.ps1 dev-test role-test eastus
